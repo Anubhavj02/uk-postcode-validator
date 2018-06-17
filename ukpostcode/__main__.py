3cc50e404a68988771d5192734353076382989ac
Anubhav Jain <32910680+Anubhavj02@users.noreply.github.com>
2018-06-17 16:25:48 +0100

    Update __main__.py

--- ukpostcode/__main__.py
+++ ukpostcode/__main__.py
@@ -6,7 +6,7 @@ from ukpostcode.postcode import Postcode
 
 if __name__ == '__main__':
 
-    # Get the input postcode string as the last parameter from the commandline
+      # Get the input postcode string as the last parameter from the commandline
     postcode_str = sys.argv[-1]
 
     print("\nInput Post Code String: " + postcode_str)
@@ -17,13 +17,6 @@ if __name__ == '__main__':
     postcode.format_postcode(postcode_str)
     print("Message:", postcode.message)
     print("Formatted post code:", postcode.formatted_postcode)
-
-    # Validating the post code
-    print("\n\n######### Validating Postcode #########")
-    postcode = Postcode()
-    postcode.validate_postcode(postcode_str)
-    print("Status:", postcode.valid)
-    print("Message:", postcode.message)
 
     # Splitting the post code
     print("\n\n######### Formatting, Validating and Splitting Postcode #########")
